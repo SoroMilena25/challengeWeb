f64f865ff2ac679ad151eec77ed41b2b32e5daef
SoroMilena25 <soro.milena25@gmail.com>
2024-12-11 20:48:34 +0100

    update

---
+++ front/src/components/NavbarProfil.vue
@@ -0,0 +1,38 @@
+<script>
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+</script>
+
+<template>
+    <div id="navBarProfil">
+        <div>
+            <p>Photo</p>
+            <p>Pseudo</p>
+        </div>
+        <div>
+            <p>Abonnés</p>
+            <p>10</p>
+        </div>
+        <div>
+            <p>Abonnements</p>
+            <p>22</p>
+        </div>
+    </div>
+</template>
+
+<style>
+#navBarProfil{
+    display: flex;
+    flex-direction: row;
+    border: 1px solid black;
+    justify-content: space-around;
+    padding-top: 2%;
+}
+
+#navBarProfil div{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+</style>
---
+++ front/src/views/Profil.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <navBarProfil />
+    </div>
+</template>
+  
+<script>
+import NavBarProfil from "@/components/NavBarProfil.vue"
+
+
+export default {
+  name: "Profil",
+  components: {
+      NavBarProfil
+  }
+};
+</script>
+  
