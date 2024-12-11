295732c97a392bf0d834c734a74dac857a7d002d
SoroMilena25 <soro.milena25@gmail.com>
2024-12-11 22:17:15 +0100

    update page profil

--- front/src/views/Profil.vue
+++ front/src/views/Profil.vue
@@ -1,17 +1,42 @@
 <template>
     <div>
-      <navBarProfil />
+      <Navbar />
+      <NavBarProfil />
+      <Footer />
+      <!-- Bouton flottant -->
+      <BoutonFloat @open-modal="showModal = true" />
+      <!-- Modal pour publier un post -->
+      <Popup
+        v-if="showModal"
+        :isVisible="showModal"
+        @close="showModal = false"
+        @publish="addPost"
+      />
     </div>
 </template>
   
 <script>
-import NavBarProfil from "@/components/NavBarProfil.vue"
+import Navbar from "@/components/Navbar.vue";
+import NavBarProfil from "@/components/NavBarProfil.vue";
+import Footer from "@/components/Footer.vue";
+import Popup from "@/components/Popup.vue";
+import BoutonFloat from "@/components/BoutonFloat.vue";
 
 
 export default {
   name: "Profil",
   components: {
-      NavBarProfil
+      Navbar,
+      NavBarProfil,
+      Footer,
+      Popup,
+      BoutonFloat
+  },
+  data() {
+    return {
+      showModal: false, // État pour gérer l'affichage du modal
+      posts: [],
+    };
   }
 };
 </script>
